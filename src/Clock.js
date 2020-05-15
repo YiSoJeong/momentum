818e37ef45a3e90271b24fd9aa52f6a9c4f73069
Yi SoJeong <soj980312@gmail.com>
2020-05-15 11:39:07 +0900

    Update time with setInterval, Format time string

--- src/Clock.js
+++ src/Clock.js
@@ -5,11 +5,15 @@ const getTime = () => {
   const date = new Date();
   const minutes = date.getMinutes();
   const hours = date.getHours();
-  clockTitle.innerText = `${hours}:${minutes}`;
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 };
 
 const init = () => {
   getTime();
+  setInterval(getTime, 1000);
 };
 
 init();
